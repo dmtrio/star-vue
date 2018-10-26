25b2cd862c757bfab0ba8db4975c77a98f0e3e72
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2018-10-26 15:23:36 -0500

    clean up

---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main">
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: {
+    title: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: green;
+}
+</style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Main from '@/components/Main.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe('Main.vue', () => {
+  it('renders props.title when passed', () => {
+    const title = 'new message'
+    const wrapper = shallowMount(Main, {
+      propsData: { title }
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.text()).toMatch(title)
   })
 })
